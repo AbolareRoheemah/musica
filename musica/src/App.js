1bdbdfe140f6833766ceb7ca935dffa7fe1a7008
Abolare Roheemah <roheemahabo@gmail.com>
2022-10-26 22:32:01 +0100

    playlist page

--- musica/src/App.js
+++ musica/src/App.js
@@ -21,7 +21,7 @@ function App() {
         </div>
         <div className='app-body'>
           <Routes>
-            <Route path='/' element={ <Dashboard /> } />
+            <Route exact={true} path='/' element={ <Dashboard /> } />
             <Route path='/playlists' element={ <Playlists /> } />
             <Route path='/profile' element={ <Profile /> } />
             <Route path='/radio' element={ <Radio /> } />
